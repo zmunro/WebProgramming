c81dc37336883805855b8cc9f8197ad1738c4a06
Ming Chow <mchow@cs.tufts.edu>
2017-10-27 15:02:43 -0400

    Posted practice quiz 2

--- examples/nodejs/simpleexpress/app.js
+++ examples/nodejs/simpleexpress/app.js
@@ -1,15 +1,10 @@
 // Taken from http://expressjs.com/4x/api.html
 var express = require('express');
-
-// Required for processing GET and POST parameters
-var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser.json()); // using body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
 
 /* General form:
 
-app.HTTP_VERB('ROUTE', function(request, response) {
+app.HTTP_VERB('ROUTE_NAME', function(request, response) {
 	// Do something
 });
 */
@@ -21,12 +16,6 @@ app.get('/', function(request, response) {
 
 app.get('/pikachu', function(request, response) {
 	response.send("You've won the game!");
-});
-
-app.post('/submit', function(request, response) {
-	var username = request.body.username;
-	console.log(username);
-	response.send("Hello, you successfully sent the username " + username + "!");
 });
 
 // Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
