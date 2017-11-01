cd155ec5875fb3067410da27ede74c79a4dff6b9
Ming Chow <mchow@cs.tufts.edu>
2017-11-01 00:32:38 -0400

    Updated Node.js + Express example

--- examples/nodejs/simpleexpress/app.js
+++ examples/nodejs/simpleexpress/app.js
@@ -2,10 +2,6 @@
 var express = require('express');
 var app = express();
 
-app.get('/landmarks', function(request, response) {
-	response.send('{"name":"Dracula\'s Castle","location":"Transylvania, Romania"}');
-});
-
 app.get('/', function(request, response) {
 	response.send("Hello world");
 });
@@ -13,10 +9,6 @@ app.get('/', function(request, response) {
 app.get('/pikachu', function(request, response) {
 	console.log(request);
 	response.send("You've won the game!");
-});
-
-app.post('/sendLocation', function(request, response) {
-	response.send('{"people":[],"landmarks":[]}');
 });
 
 // Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
