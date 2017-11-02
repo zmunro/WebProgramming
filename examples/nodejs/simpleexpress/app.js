ca1d8ec00a78cd2322b6e6c346e100d756936ab7
Ming Chow <mchow@cs.tufts.edu>
2017-11-02 12:38:26 -0400

    Updated simpleexpress example so POST route can read data client sent

--- examples/nodejs/simpleexpress/app.js
+++ examples/nodejs/simpleexpress/app.js
@@ -1,6 +1,11 @@
 // Taken from http://expressjs.com/4x/api.html
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser'); // Required if we need to use HTTP post parameters
+// See https://stackoverflow.com/questions/5710358/how-to-get-post-query-in-express-node-js
+app.use(bodyParser.json());
+// See https://stackoverflow.com/questions/25471856/express-throws-error-as-body-parser-deprecated-undefined-extended
+app.use(bodyParser.urlencoded({ extended: true })); // Required if we need to use HTTP post parameters
 
 app.get('/landmarks', function(request, response) {
 	response.send('{"name":"Dracula\'s Castle","location":"Transylvania, Romania"}');
@@ -16,7 +21,11 @@ app.get('/pikachu', function(request, response) {
 });
 
 app.post('/sendLocation', function(request, response) {
-	response.send('{"people":[],"landmarks":[]}');
+	var theLogin = request.body.login;
+	var theLat = request.body.lat;
+	var theLng = request.body.lng;
+	var returnObject = {"login":theLogin,"lat":theLat,"lng":theLng};
+	response.send(returnObject);
 });
 
 // Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
